4cbf1351533814c15b1ef3f77ec71c7c4c21f60a
emmnesty <elyjhonatan@hotmail.com>
2022-07-08 14:25:00 -0300

    Começando a trabalhar com o padrão MVC

--- src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -8,20 +8,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/oi")
-public class OlaMundo extends HttpServlet {
+//oi
+//@WebServlet(urlPatterns="/oi")
+public class OiMundoServlet extends HttpServlet {
+	
+	public OiMundoServlet() {
+		System.out.println("Criando Oi Mundo Servlet");
+	}
 	
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException  {
-		
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
 		PrintWriter out = resp.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("Oi Mundo!");
+		out.println("oi mundo, parabens vc escreveu o primeiro servlets.");
 		out.println("</body>");
 		out.println("</html>");
 		
-		System.out.println("O servlet tá on!");
+		System.out.println("o servlet OiMundoServlet foi chamado");
 	}
-
 }
