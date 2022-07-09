b9d7edf3e752ad72eb16ef95a9a18820350b09f8
emmnesty <elyjhonatan@hotmail.com>
2022-07-09 16:56:26 -0300

    Adicionando Login e Senha

--- src/br/com/alura/gerenciador/acao/Acao.java
+++ src/br/com/alura/gerenciador/acao/Acao.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public interface Acao {
 
 	String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
