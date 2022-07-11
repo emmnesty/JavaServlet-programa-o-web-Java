0c3dac88f0f04de8e39e21be2d0067fae38db5d1
Jhonatan Maciel <82352924+emmnesty@users.noreply.github.com>
2022-07-11 17:39:08 -0300

    Adicionando Maven

---
+++ src/br/com/alura/gerenciador/teste.java
@@ -0,0 +1,17 @@
+package br.com.alura.gerenciador;
+
+public class teste {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+	public class limitaString {
+		private String descricao;
+
+		
+	}
+	
+		  
+}
